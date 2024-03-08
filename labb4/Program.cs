498e043bc861f9ae25fed37e31dc4b3ab7090dd0
kir1llmalyshev <162722643+kir1llmalyshev@users.noreply.github.com>
2024-03-08 18:36:50 +0300

    Create Program.cs

---
+++ labb4/Program.cs
@@ -0,0 +1,27 @@
+using labb4.DB;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<CContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("DB"));
+});
+
+builder.Services.AddTransient<IRepo, Repo>();
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.MapControllers();
+
+app.Run();
